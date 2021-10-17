bf264b28cd66c685003045bb4de2b1d4eb6ea679
Hungry Man <katsu.matsushita@gmail.com>
2021-10-16 21:21:17 -0400

    Added the ability to submit posts. The post submission modal is now working. The new posts confirmed to show up on the dashboard and the homepage.

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -11,4 +11,19 @@ router.get("/:postID", async (req, res) => {
     };
 });
 
+// in dashboard.handlebars, the HTML form is used to send the data to this route, and does not need its own eventListener
+// this makes the code simpler to maintain and more efficient
+router.post("/", async (req, res) => {
+    try {
+        console.log(req.body.title);
+        console.log(req.body.content);
+        req.body.user_id = req.session.user_id;
+        //console.log(req.body);
+        const postData = await Post.create(req.body);
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(500).json("Error when attempting to post");
+    };
+});
+
 module.exports = router;
